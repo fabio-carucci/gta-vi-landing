af0efe04740056a0f7f42da431f3f6d1ea82b214
Fabio Carucci <garu2000@live.it>
2025-08-03 13:34:42 +0200

    feat: final video

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Jason from "./sections/Jason";
 import SecondVideo from "./sections/SecondVideo";
 import Lucia from "./sections/Lucia";
 import Postcard from "./sections/Postcard";
+import Final from "./sections/Final";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -21,6 +22,7 @@ const App = () => {
       <SecondVideo />
       <Lucia />
       <Postcard />
+      <Final />
     </main>
   );
 };
